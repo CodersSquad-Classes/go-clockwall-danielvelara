60c8306fec171f7514f252e551fd0c9235db8a43
Daniel Velázquez Lara <13500134+danielvelara@users.noreply.github.com>
2022-05-10 22:30:22 -0500

    Done

--- clockWall.go
+++ clockWall.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"strings"
+	"os"
+	"time"
+)
+
 func main() {
-	// Let's start the fun
+	if len(os.Args) == 1 {
+		fmt.Printf("Error")
+		return
+	}
+	for _, input := range os.Args[1:] {
+		splittedInput := strings.Split(input, "=")
+
+		// fmt.Printf("%s", splittedInput)
+
+		if len(splittedInput) != 2 {
+			fmt.Printf("Bad input\n")
+			return
+		}
+
+		tz := splittedInput[0]
+		port := splittedInput[1]
+
+		// fmt.Printf("%s", tz)
+
+		conn, err := net.Dial("tcp", port)
+
+		if err != nil {
+			fmt.Printf("%s", err)
+			return
+		}
+
+		
+		go Clock{
+			tz:	tz,
+			connection: conn,
+		}.handleConnection()
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
+
+
+type Clock struct {
+	tz      string
+	connection net.Conn
+}
+
+func (clock Clock) handleConnection() {
+	for {
+		data := make([]byte, 11)
+		_, err := clock.connection.Read(data)
+		if err != nil {
+			clock.connection.Close()
+			fmt.Printf("%s", err)
+			return
+
+		} else {
+			fmt.Printf("%s: %s", clock.tz, data)
+		}
+	}
 }
